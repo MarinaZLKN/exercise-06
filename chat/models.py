34f8337f1ef4cfd216455eef8bcfca0e9bfd52c4
Marina <marina.zilkina@gmail.com>
2022-11-15 21:45:33 +0200

    message model

--- chat/models.py
+++ chat/models.py
@@ -8,3 +8,10 @@ class Room(models.Model):
     name = models.CharField(max_length=100, default=None)
     updated = models.DateTimeField(auto_now=True)
     users = models.ManyToManyField(User, related_name='room_to_user')
+
+
+class Message(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='message_to_room')
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_to_user')
