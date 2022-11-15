4508e5fa48939e773367438404c7babc71834300
Marina <marina.zilkina@gmail.com>
2022-11-15 02:06:14 +0200

    Models, rooms displaying the romms on the main page

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    name = models.CharField(max_length=100, default=None)
+    updated = models.DateTimeField(auto_now=True)
+    users = models.ManyToManyField(User, related_name='room_to_user')
--- chat/views.py
+++ chat/views.py
@@ -3,14 +3,18 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from chat import forms
+from chat import forms, models
 
 
 def index(request):
     user = request.user
     if user.is_authenticated:
+        room_list = models.Room.objects.filter(users__pk=user.pk).order_by('-updated')
         user_list = User.objects.exclude(pk=request.user.pk)
-        return render(request, 'chat/index.html', {'user_list': user_list})
+        return render(request, 'chat/index.html', {
+            'room_list': room_list,
+            'user_list': user_list
+        })
     else:
         return redirect('chat:login_view')
 
@@ -53,11 +57,19 @@ def room_view(request):
 
 
 def room_add(request):
-    user_list = User.objects.exclude(pk=request.user.pk)
+    user_current = request.user
+    user_list = User.objects.exclude(pk=user_current.pk)
     if request.method == 'POST':
         form = forms.RoomAddForm(request.POST, user_list=user_list)
         if form.is_valid():
-            return redirect('chat:room_view')
+            room_name = request.POST['name']
+            room_users = request.POST.getlist('room_members')
+            room_new = models.Room(name=room_name)
+            room_new.save()
+            room_new.users.add(user_current)
+            for user_pk in room_users:
+                room_new.users.add(user_pk)
+            return redirect('chat:index')
         else:
             return render(request, 'chat/room_add.html', {'form': form})
     else:
